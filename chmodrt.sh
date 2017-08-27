c3e46c009eceda2d13509758027adb6e4bdcc484
Steven Ortiz <sortiz832@gmail.com>
2017-08-26 22:32:26 -0500

    Updated old script

--- chmodrt.sh
+++ chmodrt.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
+name="chmodrt"
 
 # Validate the arguments (count only)
 if [ "$#" -lt 3 ]; then
-    printf "Usage: ${0} TYPE MODE PATH\n\n"
+    printf "Usage: $name TYPE MODE PATH\n\n"
     printf "Types:\n"
     printf "    -d  Change the mode of directories\n"
     printf "    -f  Change the mode of files\n"
@@ -11,17 +12,17 @@ fi
 
 # Authorize absolute paths (all or nothing)
 for path in "${@:3}"; do
-    if [[ "${path}" = /* ]] || [[ "${path}" = [A-Za-z]:/* ]]; then
+    if [[ "$path" = /* ]] || [[ "$path" = [A-Za-z]:/* ]]; then
         while true; do
-            read -p "${0}: ${path} is absolute - continue? [y/n] " yn
+            read -p "$name: $path is absolute - continue? [y/n] " yn
             case $yn in
                 [Yy]* ) break;;
-                [Nn]* ) echo "${0}: abort"; exit 0;;
+                [Nn]* ) printf "$name: abort\n"; exit 0;;
             esac
         done
     fi
 done
 
 # Evaluate the type and execute
-type=`echo "${1}" | sed "s/-//g"`
-find ${@:3} -type ${type} -exec chmod ${2} {} \;
+type=`printf -- $1 | sed "s/-//g"`
+find ${@:3} -type $type -exec chmod $2 {} \;
